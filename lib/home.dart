153d0c6165e4fea704fea63f58ca60af71fe293d
Soumen <soumen@spare8.com>
2022-06-21 08:16:23 +0530

    Refactored Home Screen

--- lib/home.dart
+++ lib/home.dart
@@ -2,7 +2,7 @@ import 'dart:async';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:resume_builder/main.dart';
-
+import 'package:google_fonts/google_fonts.dart';
 
 
 
@@ -30,9 +30,331 @@ class home_state extends State<home>
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white38,
-        body: Center(
-          child: Text("Home"),
-        ),
+        body: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+
+              // Row -> menu btn and search btn (Coloumn child 1)
+              new Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children:<Widget> [
+                  new Container(
+                    padding: const EdgeInsets.fromLTRB(10,0,20,0),
+                       child: new Image.asset(
+                       'assets/images/menu.png',
+                         height: 40.0,
+                         fit: BoxFit.cover,
+                        ),
+                      ),
+
+
+                  new Container(
+                    padding: const EdgeInsets.fromLTRB(0,10,20,0),
+                        child: new Image.asset(
+                          'assets/images/search.png',
+                          height: 35.0,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+
+                ],
+              ),
+
+              new Container(
+                //color: Colors.amberAccent,
+                height: 140,
+                  margin: const EdgeInsets.fromLTRB(10,10,10,0),
+                decoration: BoxDecoration(
+                  color: const Color(0xffd4e3ea),
+                  borderRadius: BorderRadius.all(Radius.circular(25)),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    new Container(
+                      child: new Image.asset(
+                        'assets/images/profile.png',
+                      ),
+                    ),
+
+                    new Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: <Widget>[
+
+                        new Text("Make Your Resume",
+                          style: GoogleFonts.poppins(
+                            textStyle: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)
+                          )
+
+                        ),
+
+                        new Text("Resume Builder allows you to\n customize your own resumes\n with various categorization",
+                            style: GoogleFonts.poppins(
+                                textStyle: TextStyle(fontSize: 14, fontWeight: FontWeight.normal)
+                            ),
+                          maxLines: 4,
+
+                        )
+
+                      ]
+
+
+                    )
+
+
+                  ]
+                )
+
+              ),
+
+
+
+              new Container(
+                  margin: const EdgeInsets.fromLTRB(10,30,20,0),
+                  child: new Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+
+                      children: <Widget>[
+
+                        new Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+
+                            new Text("Make Your Resume",
+                                style: GoogleFonts.poppins(
+                                    textStyle: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)
+                                )
+                            ),
+
+                                new Text("See All",
+                                style: GoogleFonts.poppins(
+                                    textStyle: TextStyle(fontSize: 10)
+                                )
+                                )
+
+                          ]
+                        ),
+
+
+                             new SizedBox(
+                               height: 150,
+                             child: ListView(
+                                 scrollDirection: Axis.horizontal,
+                               padding: const EdgeInsets.all(8),
+                                 children: <Widget>[
+
+                                   new Container(
+                                     width: 150,
+                                     decoration: BoxDecoration(
+                                       color: const Color(0xffd4e3ea),
+                                       borderRadius: BorderRadius.all(Radius.circular(25)),
+                                     ),
+                               margin: const EdgeInsets.fromLTRB(0,0,10,0),
+                               child: new Image.asset(
+                               'assets/images/profile.png',
+                               height: 35.0,
+                               fit: BoxFit.cover,
+                             ),
+                                   ),
+
+                                   new Container(
+                                       width: 150,
+                                     decoration: BoxDecoration(
+                                       color: const Color(0xffd4e3ea),
+                                       borderRadius: BorderRadius.all(Radius.circular(25)),
+                                     ),
+                                     margin: const EdgeInsets.fromLTRB(0,0,10,0),
+                                     child: new Image.asset(
+                                       'assets/images/profile.png',
+                                       height: 35.0,
+                                       fit: BoxFit.cover,
+                                     ),
+                                   ),
+
+                                   new Container(
+                                   width: 150,
+                                     decoration: BoxDecoration(
+                                       color: const Color(0xffd4e3ea),
+                                       borderRadius: BorderRadius.all(Radius.circular(25)),
+                                     ),
+                                     margin: const EdgeInsets.fromLTRB(0,0,10,0),
+                                     //padding: const EdgeInsets.all(20),
+                                     child: new Image.asset(
+                                       'assets/images/profile.png',
+                                       fit: BoxFit.cover,
+                                     ),
+                                   ),
+
+                                   new Container(
+                                     width: 150,
+                                     decoration: BoxDecoration(
+                                       color: const Color(0xffd4e3ea),
+                                       borderRadius: BorderRadius.all(Radius.circular(25)),
+                                     ),
+                                     margin: const EdgeInsets.fromLTRB(0,0,10,0),
+                                     //padding: const EdgeInsets.all(20),
+                                     child: new Image.asset(
+                                       'assets/images/profile.png',
+                                       fit: BoxFit.cover,
+                                     ),
+                                   ),
+
+                                   new Container(
+                                     width: 150,
+                                     decoration: BoxDecoration(
+                                       color: const Color(0xffd4e3ea),
+                                       borderRadius: BorderRadius.all(Radius.circular(25)),
+                                     ),
+                                     margin: const EdgeInsets.fromLTRB(0,0,10,0),
+                                     //padding: const EdgeInsets.all(20),
+                                     child: new Image.asset(
+                                       'assets/images/profile.png',
+                                       fit: BoxFit.cover,
+                                     ),
+                                   ),
+
+
+
+                                 ]
+
+                             )),
+
+
+
+
+
+
+                      ]
+                  )
+
+
+              ),
+
+
+            new Container(
+              margin: const EdgeInsets.fromLTRB(10,30,20,0),
+              child: new Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+
+                  children: <Widget>[
+
+                    new Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+
+                          new Text("Make Your Resume",
+                              style: GoogleFonts.poppins(
+                                  textStyle: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)
+                              )
+                          ),
+
+                          new Text("See All",
+                              style: GoogleFonts.poppins(
+                                  textStyle: TextStyle(fontSize: 10)
+                              )
+                          )
+
+                        ]
+                    ),
+
+
+                    new SizedBox(
+                        height: 150,
+                        child: ListView(
+                            scrollDirection: Axis.horizontal,
+                            padding: const EdgeInsets.all(8),
+                            children: <Widget>[
+
+                              new Container(
+                                width: 150,
+                                decoration: BoxDecoration(
+                                  color: const Color(0xffd4e3ea),
+                                  borderRadius: BorderRadius.all(Radius.circular(25)),
+                                ),
+                                margin: const EdgeInsets.fromLTRB(0,0,10,0),
+                                child: new Image.asset(
+                                  'assets/images/profile.png',
+                                  height: 35.0,
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+
+                              new Container(
+                                width: 150,
+                                decoration: BoxDecoration(
+                                  color: const Color(0xffd4e3ea),
+                                  borderRadius: BorderRadius.all(Radius.circular(25)),
+                                ),
+                                margin: const EdgeInsets.fromLTRB(0,0,10,0),
+                                child: new Image.asset(
+                                  'assets/images/profile.png',
+                                  height: 35.0,
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+
+                              new Container(
+                                width: 150,
+                                decoration: BoxDecoration(
+                                  color: const Color(0xffd4e3ea),
+                                  borderRadius: BorderRadius.all(Radius.circular(25)),
+                                ),
+                                margin: const EdgeInsets.fromLTRB(0,0,10,0),
+                                //padding: const EdgeInsets.all(20),
+                                child: new Image.asset(
+                                  'assets/images/profile.png',
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+
+                              new Container(
+                                width: 150,
+                                decoration: BoxDecoration(
+                                  color: const Color(0xffd4e3ea),
+                                  borderRadius: BorderRadius.all(Radius.circular(25)),
+                                ),
+                                margin: const EdgeInsets.fromLTRB(0,0,10,0),
+                                //padding: const EdgeInsets.all(20),
+                                child: new Image.asset(
+                                  'assets/images/profile.png',
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+
+                              new Container(
+                                width: 150,
+                                decoration: BoxDecoration(
+                                  color: const Color(0xffd4e3ea),
+                                  borderRadius: BorderRadius.all(Radius.circular(25)),
+                                ),
+                                margin: const EdgeInsets.fromLTRB(0,0,10,0),
+                                //padding: const EdgeInsets.all(20),
+                                child: new Image.asset(
+                                  'assets/images/profile.png',
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+
+
+
+                            ]
+
+                        )),
+
+
+
+
+
+
+                  ]
+              )
+            ),
+
+
+              ],
+          )
+        )
 
 
 
--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -4,6 +4,7 @@ import 'package:resume_builder/main.dart';
 
 import 'main.dart';
 import 'main_screen.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class splash_screen extends StatefulWidget
 {
@@ -18,20 +19,42 @@ class splash_state extends State<splash_screen>
   @override
   void initState()
   {
-    super.initState();
     navigate_to_home();
+    super.initState();
+
   }
   
   navigate_to_home() async
   {
-    await Future.delayed(Duration(milliseconds: 3500),() {});
+    await Future.delayed(Duration(milliseconds: 7500),() {});
     Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => main_screen()));
   }
+
+  final text_style = TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text("Splash Screen"),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            new Container(
+              child: Image.asset('assets/images/profile.png',
+              height: 200,
+              width: 200)
+            ),
+            SizedBox(height: 100),
+            Text("Resume Builder",
+            style: GoogleFonts.poppins(
+              textStyle: text_style
+            )
+            )
+            // child: Image.asset('assets/images/profile.png',
+            //     height: 200,
+            //     width: 200),
+          ],
+        )
+       ,
       )
     );
   }
