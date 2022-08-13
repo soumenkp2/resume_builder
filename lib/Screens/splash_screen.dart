be3ecad9edcf2a32371736e5517a2caf14122624
Tejash <tejashth1103@gmail.com>
2022-08-14 00:36:44 +0530

    added shared preference

--- lib/Screens/splash_screen.dart
+++ lib/Screens/splash_screen.dart
@@ -2,10 +2,12 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:resume_builder/main.dart';
 import 'package:resume_builder/Screens/onboarding_screen.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import '../main.dart';
 import 'main_screen.dart';
 import 'package:google_fonts/google_fonts.dart';
+
 
 class splash_screen extends StatefulWidget
 {
@@ -24,11 +26,24 @@ class splash_state extends State<splash_screen>
     super.initState();
 
   }
-  
+  Future checkFirstSeen() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    bool _seen = (prefs.getBool('seen') ?? false);
+
+    if (_seen) {
+      Navigator.of(context).pushReplacement(
+          new MaterialPageRoute(builder: (context) => new main_screen(0)));
+    } else {
+      await prefs.setBool('seen', true);
+      Navigator.of(context).pushReplacement(
+          new MaterialPageRoute(builder: (context) => new onboarding_screen()));
+    }
+  }
   navigate_to_home() async
   {
     await Future.delayed(Duration(milliseconds: 7500),() {});
-    Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => onboarding_screen()));
+    checkFirstSeen();
+    // Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => onboarding_screen()));
   }
 
   final text_style = TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
